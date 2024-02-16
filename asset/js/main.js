b9cfc524fcbfa413305fd739d02314185b835ffe
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-16 16:10:27 +0100

    esercizio 2c

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,15 +1,24 @@
-let number = "100"
+let number = 100 /* number */
 
-console.log('100')
+console.log(100);
 
-const PI = '3.14'
+const PI = 3.14
 
-console.log('3.14')
-
-/*
-const PI = '3.14' = const PI = '3.14'
- PI è gia stato dichiarato */
+console.log(3.14);
 
 
+//const PI = '3.1416'
 
+
+//è gia stato dichiarato PI 
+
+
+
+let radius = 9
+
+console.log(radius * 2 * PI);
+
+let circle = 56.52
+
+console.log(56.52)
 
