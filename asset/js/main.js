60f5260703f18e7c1a0151e8579df9e82513705f
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-19 18:09:34 +0100

    adjustments

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,30 +1,34 @@
+// esercizio 1
 let number = 100 /* number */
 
 console.log(100);
-
+// esercizio 2
 const PI = 3.14
 
-console.log(3.14);
+console.log(PI);
 
 
-//const PI = '3.1416'
+//const PI = '3.1416' esercizio 2/B
 
 
 //è gia stato dichiarato PI 
 
 
-
+// esercizio 2/C
 let radius = 9
 
-console.log(radius * 2 * PI);
+console.log((radius * 2) * PI);
 
 let circle = 56.52
 
 console.log(56.52);
 
+// esercizio 3
+
+
 let name = 'Carlo' /* string */
 
-let marco = 'Carlo'
+name = 'Marco'
 
-console.log('name');
+console.log(name);
 
